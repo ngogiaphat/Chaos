a45050882bef53d2bafe6b2ee9dc3370548e48a5
ngogiaphat <ngogiaphat0802@gmail.com>
2023-04-30 19:19:51 +0700

    Change location files

--- src/C#/method.cs
+++
@@ -1,8 +0,0 @@
-private void MyTask(string[] files)
-{
-  foreach (string file in files)
-  {
-    // a time consuming operation with a file (compression, encryption etc.)
-    Thread.Sleep(1000);
-  }
-}
