fc234f78b93457352865972d3f5df262b22e5aa9
ngogiaphat <ngogiaphat0802@gmail.com>
2023-04-30 15:26:57 +0700

    Pointer and Singly Linked List in C/C++ 2

--- src/C/SinglyLinkedList/SinglyLinkedList.c
+++ src/C/SinglyLinkedList/SinglyLinkedList.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node *next;
+};
+struct Node *head = NULL; //Initialize empty list
+void insert(int value) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node)); //Initialize new Node
+    newNode->data = value;
+    newNode->next = NULL;
+    //If list is empty then set new node as first node
+    if (head == NULL) { 
+        head = newNode;
+    }
+    //Otherwise add new node at the end of the list 
+    else { 
+        struct Node *temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void printList() {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
 int main() {
-   int num1 = 10, num2 = 20;
-   int *ptr1 = &num1;
-   int *ptr2 = &num2;
-   printf("Value of num1 = %d\n", num1); //Print the value of num1 before swap
-   printf("Value of num2 = %d\n", num2); //Print the value of num2 before swap
-   //Use 2 pointers to swap the values of 2 variables num1 and num2
-   int *temp;
-   temp = ptr1;
-   ptr1 = ptr2;
-   ptr2 = temp;
-   printf("Value of num1 = %d\n", *ptr1); //Print value of num1 after swap 
-   printf("Value of num2 = %d\n", *ptr2); //print value of num2 after swap
-   return 0;
+    insert(1);
+    insert(2);
+    insert(3);
+    insert(4);
+    insert(5);
+    printf("Danh sách liên kết đơn: ");
+    printList();
+    return 0;
 }
