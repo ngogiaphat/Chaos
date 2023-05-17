8789ee1bd951712b385d6b94dd3e1244c269dbea
ngogiaphat <ngogiaphat0802@gmail.com>
2023-05-17 16:59:43 +0700

    training for Sorting Algorithms

---
+++ src/Python/Algorithm/Sort/QuickSort.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = []
+        right = []
+        for i in arr[1:]:
+            if i < pivot:
+                left.append(i)
+            else:
+                right.append(i)
+        return quick_sort(left) + [pivot] + quick_sort(right)
